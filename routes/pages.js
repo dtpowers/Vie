c85e85ed48b517cf76c4fdc66a106fc241947f02
donovan powers <donovan powers>
2016-12-02 14:00:02 -0500

    fixed linking and routes

--- routes/pages.js
+++ routes/pages.js
@@ -2,7 +2,11 @@
 
 exports.init = function(app) {
   app.get('/', function(req, res) {
-    res.sendfile("views/index.html");
+    res.sendfile("views/homepage.html");
+  });
+
+  app.get('/addpage', function(req, res) {
+    res.sendfile("views/add_page.html");
   });
 
   app.post('/addCommitment', function(req, res) {
